41462b05f81049ef146e9685c2d9cdc7915d2870
Lola <loladupel@icloud.com>
2024-02-16 13:57:20 +0100

    display

--- components/Home.js
+++ components/Home.js
@@ -5,7 +5,11 @@ function Home() {
   return (
     <div>
       <main className={styles.main}>
+        {/* div avec tout dedans */}
+
         <div className={styles.left}>
+         {/* div left avec icon, userprofile, logout */}
+
             <div className={styles.icon}>
             <Image
               src="/poulpicon.png"
@@ -20,25 +24,61 @@ function Home() {
               <p>@username</p>
               <button>Logout</button>
             </div>
+
         </div>
-        <div className={styles.touslestweets}>
-        <h1 className={styles.title}>
+
+        <div className={styles.mid}>
+          {/* div mid avec tout */}
+
+         <div className={styles.headmid}>
+          {/* div pour éccrire tweet title, input, compteur caractères et bouton */}
+
+         <h1 className={styles.title}>
           Home
-        </h1>
-        <div className={styles.mytweet}>
-        <input type="text" placeholder="What's up?" id="mytweet" />
-        <p>0/280</p>
-        <button id="register" >Tweet</button>
+         </h1>
+
+         <div className={styles.mytweet}>
+          {/* div avec input compteur et bouton */}
+
+         <input type="text" placeholder="What's up?" id="mytweet" />
+         <p>0/280</p>
+         <button id="register" >Tweet</button>
+         </div>
         </div>
-        <div className={styles.tweet}>
+
+        <div className={styles.tweets}>
+           {/* div tous les tweets */}
+
+         <div className={styles.onetweet}>
+           {/* div avec un seul tweet */}
+
           <div className={styles.userinfos}>
             <p>Profpic name @name</p>
+          </div>
+          <div className={styles.textweet}>
             <p>lalalala cest mon tweet</p>
+          </div>
+          <div className={styles.heartbtn}>
             <button>ptitcoeuricon</button>
           </div>
 
+          </div>
         </div>
+     
         </div>
+        <div className={styles.right}>
+
+<div className={styles.trends}>
+  <h2>Trends</h2>
+</div>
+
+<div className={styles.counters}>
+  <div className={styles.counter}>
+    <h6>#lala</h6>
+    <p>x tweets</p>
+  </div>
+</div>
+</div>
       </main>
     </div>
   );
